d7a0549c19e01e48642c408978a2f3511e1e1da2
otaferreira2202-spec <otaferreira2202@gmail.com>
2026-04-15 19:33:53 -0300

    Add triangle pattern printing program
    Implement a program to print a triangle pattern based on user input for the base size.

---
+++ triangulo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+   {
+    int base = atoi(argv[1]);
+    if(argc == 2){
+        for(int i = 1; i <= base; i++)
+           {
+            for (int j = 1; j <= i; j++) 
+   {  
+                printf("*");
+            }
+            printf("\n");
+        } 
+    } else
+           {
+        printf("digite a base do triangulo");
+    }
+    
+    printf("\nFeito por Leonardo Torres (https://github.com/otaferreira2202-spec) ");
+    return 0;
+}
